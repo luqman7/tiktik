ce3a023c6fc3efa7ecbfe7f5c9a4c9bd40738bdf
luqmankacak7296 <muhamadluqmanhafizulazly@yahoo.com>
2022-08-09 17:41:29 +0800

    change next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['i0.wp.com', 'lh3.googleusercontent.com'],
